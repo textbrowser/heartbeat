526f7fbec5d83c7ff1b4a1543a3c2c81e8dcd577
textbrowser <textbrowser@gmail.com>
2023-08-05 11:28:20 -0400

    New ideas.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # heartbeat
 Periodically send GitHub important information.
+
+> export GIT-A Account
+> export GIT-T Token
--- delicious-pi.sh
+++ delicious-pi.sh
@@ -3,8 +3,10 @@
 
 while
     echo $(date '+%Y-%m-%d:%H:%M:%S:%N'; vcgencmd measure_temp) > \
-	 data/pi-temperature.txt
+	 data/pi-temperature-$(hostname -I).txt
+    git add data 2>/dev/null
     git commit -a -m "New data!"
+    git push https://${GIT-A}:${GIT-T}@github.com/textbrowser/heartbeat
     sleep 30
 do true; done
 
