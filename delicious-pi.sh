524830d58df25c3e7b894c58244e10343e08adaa
textbrowser <textbrowser@gmail.com>
2023-08-05 10:40:50 -0400

    New data!

--- delicious-pi.sh
+++ delicious-pi.sh
@@ -5,7 +5,7 @@ while
     echo $(date '+%Y-%m-%d:%H:%M:%S:%N'; vcgencmd measure_temp) > \
 	 data/pi-temperature.txt
     git commit -a -m "New data!"
-    sleep 300
+    sleep 30
 do true; done
 
 exit 0
